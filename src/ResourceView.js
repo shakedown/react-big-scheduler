e84027f4320561202cd03f66b94a73291a20cf7f
hiramzamorano <ahzamorano@gmail.com>
2021-04-13 16:36:33 -0500

    Fixes text overflow

--- src/ResourceView.js
+++ src/ResourceView.js
@@ -53,16 +53,16 @@ class ResourceView extends Component {
             }
             indents.push(indent);
 
-            let a = slotClickedFunc != undefined ? <span className="slot-cell">{indents}<a className="slot-text" onClick={() => {
+            let a = slotClickedFunc != undefined ? <span className="slot-cell">{indents}<a className="slot-text" style={{width: width}} onClick={() => {
                 slotClickedFunc(schedulerData, item);
             }}>{item.slotName}</a></span>
-                : <span className="slot-cell">{indents}<span className="slot-text header4-text">{item.slotName}</span></span>;
+                : <span className="slot-cell">{indents}<span className="slot-text header4-text overflow-text" style={{width: width - 80}}>{item.slotName}</span></span>;
             let slotItem = (
                 <div style={{width: width}}>
                     <div className="overflow-text header2-text" style={{textAlign: "left", color: '#F08421', fontSize: '14px', marginBottom:'15px'}}>
                          {item.slotIssue ? item.slotIssue : ''}
                     </div>
-                    <div className="overflow-text header2-text" style={{textAlign: "left"}}>
+                    <div className="overflow-text header2-text" style={{textAlign: "left", }}>
                     {a}
                     </div>
                      <div className="overflow-text header2-text" style={{textAlign: "left", marginTop: '15px'}}>
@@ -71,6 +71,7 @@ class ResourceView extends Component {
                 </div>
                  
             );
+            console.log(width, 'width');
 
             if(!!slotItemTemplateResolver) {
                 let temp = slotItemTemplateResolver(schedulerData, item, slotClickedFunc, width, "overflow-text header2-text");
@@ -78,7 +79,7 @@ class ResourceView extends Component {
                     slotItem = temp;
             }
 
-            let tdStyle = { height: item.rowHeight };
+            let tdStyle = { height: item.rowHeight, display: 'inline-block' };
             if(item.groupOnly) {
                 tdStyle = {
                     ...tdStyle,
